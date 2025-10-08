fc96bfb2e7d73b3ae44904286c4161c2ea5f271b
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2025-10-09 00:53:51 +0530

    Update p.py

--- p.py
+++ p.py
@@ -6,20 +6,19 @@ from io import BytesIO
 client = genai.Client()
 
 prompt = (
-    "Create a picture of my cat eating a nano-banana in a "
-        "fancy restaurant under the Gemini constellation",
-        )
+    "Create a high quality, ultra- realistic portrait of 8k, of same   woman, from the uploaded image, keep her face and features exactly the same (no face change), young women wearing colour bikini triangle cut out on front. seeing from front,showing full body pose, and background same as reference as posible ",
+)
 
-        image = Image.open("/path/to/cat_image.png")
+image = Image.open("/shiva.jpg")
 
-        response = client.models.generate_content(
-            model="gemini-2.5-flash-image",
-                contents=[prompt, image],
-                )
+response = client.models.generate_content(
+    model="gemini-2.5-flash-image",
+    contents=[prompt, image],
+)
 
-                for part in response.candidates[0].content.parts:
-                    if part.text is not None:
-                            print(part.text)
-                                elif part.inline_data is not None:
-                                        image = Image.open(BytesIO(part.inline_data.data))
-                                                image.save("generated_image.png")
+for part in response.candidates[0].content.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = Image.open(BytesIO(part.inline_data.data))
+        image.save("generated_image.png")
